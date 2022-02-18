ddb5320ec0a58783be27d31d55d38416d922ed64
MatiasMunk <matiaspersson95@gmail.com>
2022-02-18 22:25:28 +0100

    Initial commit

--- cnr.sql
+++ cnr.sql
@@ -52,3 +52,15 @@ CREATE TABLE IF NOT EXISTS `playerskins` (
   KEY `citizenid` (`citizenid`),
   KEY `active` (`active`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `outfitname` varchar(50) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `skin` text DEFAULT NULL,
+  `outfitId` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `outfitId` (`outfitId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
